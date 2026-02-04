380f91423d3e04877142bc769f8411d542f09f07
NUwUxuz <135438037+NUwUxuz@users.noreply.github.com>
2026-02-04 13:48:36 +0100

    Update info_user.tsx

--- frontend/mon-projet/src/components/info_user.tsx
+++ frontend/mon-projet/src/components/info_user.tsx
@@ -10,7 +10,7 @@ function InfoUser({email, id, password}: InfoUserProps) {
     return (
         <section className="info-user">
             <div className="principal">
-                <div className="section-email">
+                <div className="section">
                     <label htmlFor="email" className="titre">Adresse mail : </label>
                     <input
                         id="email"
@@ -23,28 +23,32 @@ function InfoUser({email, id, password}: InfoUserProps) {
                     <img src={image_edit} className="image-edit" alt="Image de modification"></img>
                 </div>
 
-                <label htmlFor="id" className="titre">ID : </label>
-                <input
-                    id="id"
-                    type="text"
-                    className="input-text"
-                    value={id}
-                    name="id"
-                    readOnly
-                />
+                <div className="section">
+                    <label htmlFor="id" className="titre">ID : </label>
+                    <input
+                        id="id"
+                        type="text"
+                        className="input-text"
+                        value={id}
+                        name="id"
+                        readOnly
+                    />
+                </div>
             </div>
 
             <div className="secondaire">
-                <label htmlFor="password" className="titre">Mot de passe : </label>
-                <input
-                    id="password"
-                    type="password"
-                    className="input-text"
-                    value={password}
-                    name="password"
-                    readOnly
-                />
-                <img src={image_edit} className="image-edit" alt="Image de modification"></img>
+                <div className="section">
+                    <label htmlFor="password" className="titre">Mot de passe : </label>
+                    <input
+                        id="password"
+                        type="password"
+                        className="input-text"
+                        value={password}
+                        name="password"
+                        readOnly
+                    />
+                    <img src={image_edit} className="image-edit" alt="Image de modification"></img>
+                </div>
             </div>
         </section>
     )
