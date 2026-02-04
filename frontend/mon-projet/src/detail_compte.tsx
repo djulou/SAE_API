ee626c9e0cfba5b13b499c789a17763cc77822eb
NUwUxuz <135438037+NUwUxuz@users.noreply.github.com>
2026-02-04 13:05:42 +0100

    Update detail_compte.tsx

--- frontend/mon-projet/src/detail_compte.tsx
+++ frontend/mon-projet/src/detail_compte.tsx
@@ -1,11 +1,28 @@
+import "./user.css"
+import InfoUser from "./components/info_user";
 
+export default
 
 function detail_compte() {
     return (
-        <div>
-            <h2>Détail du Compte</h2>
-            <p>Informations détaillées sur le compte utilisateur.</p>
-        </div>
+        <>
+            <div>
+                <h2>Détail du Compte</h2>
+                <p>Informations détaillées sur le compte utilisateur.</p>
+            </div>
+
+            <InfoUser
+                email="adressemail@truc.com"
+                id="ncxjfvdst"
+                password="motdepass123"
+            />
+
+            <button id="bouton-user">Afficher stats utilisateurs</button>
+
+            <div>
+                <button id="deconnecter">Se déconnecter</button>
+                <button id="supprimer">Supprimer le compte</button>
+            </div>
+        </>
     );
 }
-export default detail_compte;
