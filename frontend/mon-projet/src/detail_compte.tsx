be533467336bbe3334b41b3876bac4fc5f66bf28
NUwUxuz <135438037+NUwUxuz@users.noreply.github.com>
2026-02-04 13:48:14 +0100

    Update detail_compte.tsx

--- frontend/mon-projet/src/detail_compte.tsx
+++ frontend/mon-projet/src/detail_compte.tsx
@@ -6,22 +6,17 @@ export default
 function detail_compte() {
     return (
         <>
-            <div>
-                <h2>Détail du Compte</h2>
-                <p>Informations détaillées sur le compte utilisateur.</p>
-            </div>
-
             <InfoUser
                 email="adressemail@truc.com"
                 id="ncxjfvdst"
                 password="motdepass123"
             />
 
-            <button id="bouton-user">Afficher stats utilisateurs</button>
+            <button id="bouton-user" className="bouton-cliquable">Afficher stats utilisateurs</button>
 
             <div>
-                <button id="deconnecter">Se déconnecter</button>
-                <button id="supprimer">Supprimer le compte</button>
+                <button id="deconnecter" className="bouton-cliquable">Se déconnecter</button>
+                <button id="supprimer" className="bouton-cliquable">Supprimer le compte</button>
             </div>
         </>
     );
