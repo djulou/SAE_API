7b8bb30e11d883768ff500660b997ae07fdbc3cb
NUwUxuz <135438037+NUwUxuz@users.noreply.github.com>
2026-02-06 11:41:54 +0100

    Add files via upload

---
+++ frontend/mon-projet/src/components/stats_user.tsx
@@ -0,0 +1,46 @@
+import coeur from "../assets/coeur.png"
+import playlist from "../assets/playlist.png"
+
+
+type Track = {
+    user_id: number
+    track_id: number
+    nb_listening: number
+}
+
+type Genre = {
+    user_id: number
+    genre_id: number
+    genre_rate: number
+}
+
+type StatsUserProps = {
+    favoris: number
+    playlists: number
+    track_listen: Track[]
+    genre_listen: Genre[]
+}
+
+
+function StatsUser({favoris, playlists, track_listen, genre_listen}: StatsUserProps) {
+    return (
+        <section className="stat1" >
+            <div className="stat-simple">
+                <h2 className="souligner">Nombre de musique en favoris : </h2>
+                <div className="chiffre-image">
+                    <p className="chiffre-stat">{favoris}</p>
+                    <img src={coeur} id="image-favoris" alt="Image favoris"></img>
+                </div>
+            </div>
+            <div className="stat-simple">
+                <h2 className="souligner">Nombre de playlists créées : </h2>
+                <div className="chiffre-image">
+                    <p className="chiffre-stat">{playlists}</p>
+                    <img src={playlist} id="image-playlist" alt="Image playlist"></img>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default StatsUser
