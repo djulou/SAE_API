71cd61031c1adaa9a15760a36f6b9139d8324d3f
NUwUxuz <135438037+NUwUxuz@users.noreply.github.com>
2026-02-11 13:39:51 +0100

    Update Page.tsx

--- frontend/mon-projet/src/types/Page.tsx
+++ frontend/mon-projet/src/types/Page.tsx
@@ -1,1 +1,1 @@
-export type Page = "accueil" | "detail_compte" | "page_installation" | "login" | "register";
+export type Page = "accueil" | "detail_compte" | "page_installation" | "login" | "register" | 'CGU' | 'contact' | 'mentions_legales';
